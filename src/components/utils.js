6f987dc546ec505c25f1291fad9034e8c12a0240
Lyshka <53864647+Lyshka@users.noreply.github.com>
2022-07-10 23:09:45 +0300

    connect
    connect back

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,1 +1,6 @@
+import axios from "axios"
+
 export const calcTotalPrice = items => items.reduce((sum, game) => sum + game.price, 0)
+const instance = axios.create({baseURL: process.env.BASEURL || "https://vast-tor-82563.herokuapp.com"})
+
+export default instance
